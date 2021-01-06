ae3ab84a17882e161001bc62f09e93acc839f0d5
Harrisonmk <harrison.mitchell@hotmail.com>
2021-01-06 19:28:30 -0400

    Add Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./Navbar";
 
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
 
-        <h1>Hello</h1>
+       <Navbar />
 
     </div>
   );
