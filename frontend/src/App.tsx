e4a8da64d657bd2e12fe6cb57529fe73dddffa1f
Harrisonmk <harrison.mitchell@hotmail.com>
2021-01-06 21:05:21 -0400

    Add Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <div>
 
        <Navbar />
-
+       <Home />
     </div>
   );
 }
